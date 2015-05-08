6bfb4e6f1298384e2926f3b3a818fd0a489fcacb
John Mahowald <john.mahowald@gmail.com>
2015-05-08 15:36:32 -0700

    segmentation fault debug

--- app/assets/javascripts/react/models/Tweet.js
+++ app/assets/javascripts/react/models/Tweet.js
@@ -1,6 +1,50 @@
+var invariant = require("invariant");
+var _ = require("underscore");
+var __DEV__ = "production" !== process.env.NODE_ENV;
+
 var Tweet = function (options) {
-  this.id = options.id;
-  this.content = options.content;
+  this.permitted = ["id", "content"];
+  this.data = this.validate(options);
+}
+
+Tweet.prototype.id = function (id) {
+  return this.data.id;
+}
+
+Tweet.prototype.content = function (content) {
+  return this.data.content;
+}
+
+Tweet.prototype.validate = function (options) {
+  var keys = Object.keys(options);
+  var tweet = this;
+
+  this._validateOnlyPermittedKeys(keys);
+  this._validatesRequiredKeys(keys);
+
+  return options
+}
+
+Tweet.prototype._validateOnlyPermittedKeys = function (keys) {
+  var tweet = this;
+
+  for (var i = 0; i < keys.length; i ++) {
+    invariant(
+        _.contains(tweet.permitted, keys[i]),
+        "invalid argument '%s' given", 
+        keys[i])
+  }
+}
+
+Tweet.prototype._validatesRequiredKeys = function (keys) {
+  var tweet = this;
+
+  for (var j = 0; j < this.permitted.length; j ++) {
+    invariant(
+        _.contains(keys, tweet.permitted[j]),
+        "missing key '%s'",
+        tweet.permitted[j]);
+  }
 }
 
 module.exports = Tweet;
--- app/assets/javascripts/react/models/__tests__/tweet_spec.js
+++ app/assets/javascripts/react/models/__tests__/tweet_spec.js
@@ -1,16 +1,37 @@
 jest.dontMock("../Tweet");
+jest.dontMock("invariant");
+jest.dontMock("underscore");
+
 var Tweet = require("../Tweet");
 
 describe("tweet model", function () {
-  it("", function () {
+  it("can initialize a new tweet", function () {
     var data = {
       id: 1,
-      content: "first tweet"
+      content: "first tweet",
     }
 
     var tweet = new Tweet(data);
 
-    expect(tweet.id).toBe(1);
-    expect(tweet.content).toBe("first tweet");
+    expect(tweet.id()).toBe(1);
+    expect(tweet.content()).toBe("first tweet");
+  })
+
+  describe("", function () {
+    it("raises an error when the content is not given", function () {
+      var data = {
+        content: "first tweet"
+      }
+
+      expect(function () { new Tweet(data) }).toThrow(new Error("Invariant Violation: missing key 'id'"));
+    });
+
+    it("raises an error when the id is not given", function () {
+      var data = {
+        id: 1
+      }
+
+      expect(function () { new Tweet(data) }).toThrow("Invariant Violation: missing key 'content'");
+    });
   })
 });
